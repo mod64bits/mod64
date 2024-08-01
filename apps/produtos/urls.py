a80014e03283c0c145af0e34bb8e880b27f76e6d
mod64bits <mod64bits@gmail.com>
2024-08-01 06:42:08 -0300

    Template fabricante

--- apps/produtos/urls.py
+++ apps/produtos/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from apps.produtos import fornecedoView, fabricante_view
 from apps.produtos import fabricante_view
-from apps.produtos import views
+from apps.produtos import views as produtos_views
 
 app_name = 'produtos'
 
 urlpatterns = [
-    path('fornecedores/', views.index, name='fornecedor'),
+    path('fornecedores/', produtos_views.index, name='fornecedor'),
     path('fornecedores/list/', fornecedoView.list_fornecedor, name='fornecedor_list'),
     path('fornecedor/add', fornecedoView.add_fornecedor, name='add_fornecedor'),
     path('fornecedor/<uuid:pk>/remove', fornecedoView.remove_fornecedor, name='remove_fornecedor'),
@@ -22,9 +22,9 @@ urlpatterns = [
 
     # produtos
 
-    path('', views.index, name='produtos'),
-    path('list/', views.list, name='produto_list'),
-    path('add/', views.to_add, name='produto_add'),
-    path('<uuid:pk>/edit/', views.edit, name='produto_edit'),
-    path('<uuid:pk>/remove/', views.delete, name='produto_remove'),
+    path('', produtos_views.index, name='produtos'),
+    path('list/', produtos_views.list, name='produto_list'),
+    path('add/', produtos_views.to_add, name='produto_add'),
+    path('<uuid:pk>/edit/', produtos_views.edit, name='produto_edit'),
+    path('<uuid:pk>/remove/', produtos_views.delete, name='produto_remove'),
 ]
